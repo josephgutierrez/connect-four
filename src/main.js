58a74558ff5d0824faeefef85196d54f8310494e
Joseph Gutierrez <josephjamesgutierrez@gmail.com>
2017-03-28 16:43:02 -0700

    Create player functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const initialState = {
   player: 'yellow',
   winner: false,
+  draw: false,
   board: [[0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0],
@@ -11,6 +12,14 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'CHIP_DROPPED':
+      return Object.assign({}, state, {
+        board: action.updateBoard
+      })
+    case 'PLAYER_SWITCHED':
+      return Object.assign({}, state, {
+        player: action.player
+      })
     default:
       return state
   }
@@ -18,19 +27,43 @@ const reducer = (state = initialState, action) => {
 
 let store = Redux.createStore(reducer, initialState)
 
-const $board = document.querySelector('#board')
+const dropChip = (row, col) => {
+  const {board, player} = store.getState()
+  const updateBoard = [...board]
+  
+  for (let i = board.length - 1; i >= 0; i--) {
+    if (board[i][col] == 0) {
+      if (player == 'yellow') {
+        updateBoard[i][col] = 1
+        store.dispatch({type: 'PLAYER_SWITCHED', player: 'red'})
+        break
+      } else if (player == 'red') {
+        updateBoard[i][col] = 2
+        updatePlayer = 'yellow'
+        store.dispatch({type: 'PLAYER_SWITCHED', player: 'yellow'})
+        break
+      }
+      store.dispatch({type: 'CHIP_DROPPED', updateBoard})
+    }
+  }
+}
 
 const renderGame = () => {
-
+  const $board = document.querySelector('#board')
+  $board.innerHTML = ''
   const {board} = store.getState()
 
   for (let i = 0; i < board.length; i++) {
     const $row = document.createElement('div')
     $row.classList.add('row')
     $board.appendChild($row)
+
     for (let j = 0; j < board[i].length; j++) {
       const $cell = document.createElement('div')
       $cell.classList.add('cell')
+      $cell.dataset.row = i
+      $cell.dataset.column = j
+
       if (board[i][j] == 1) {
         $cell.classList.add('yellow')
       } else if (board[i][j] == 2) {
@@ -44,5 +77,13 @@ const renderGame = () => {
 store.subscribe(renderGame)
 
 renderGame()
+
+document.addEventListener('click', (event) => {
+  if(event.target.className.includes('cell')) {
+    const $row = event.target.dataset.row
+    const $column = event.target.dataset.column
+    dropChip($row, $column)
+  }
+})
 
 console.log(store.getState())
